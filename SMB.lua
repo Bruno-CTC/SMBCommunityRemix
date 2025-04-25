b0721719fd12f3a90afdac3066f8e4979b22a5af
Nurb <70658958+Bruno-CTC@users.noreply.github.com>
2025-04-25 15:22:08 -0300

    Added glitched cards
    asd

--- SMB.lua
+++ SMB.lua
@@ -1,4 +1,8 @@
+smb = {}
 SMODS.Atlas { key = "cardSleeves", path = "atlasSleeves.png", px = 73, py = 95}
 assert(load(NFS.read(SMODS.current_mod.path .. "modules/sleeves.lua")))()
 SMODS.Atlas { key = "jokers", path = "Jokers.png", px = 71, py = 95}
 assert(load(NFS.read(SMODS.current_mod.path .. "modules/jokers.lua")))()
+
+SMODS.Atlas { key = "erroredCards", path = "atlasErrored.png", px = 66, py = 95}
+assert(load(NFS.read(SMODS.current_mod.path .. "modules/glitched.lua")))()
