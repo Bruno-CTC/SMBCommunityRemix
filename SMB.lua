65ffbb2e0c358b36d8ca3eebebff97d7ce65e082
ronitrocket <ronitrocket98@gmail.com>
2025-04-26 00:42:10 -0400

    fixed 1x sprites

--- SMB.lua
+++ SMB.lua
@@ -4,7 +4,7 @@ SMODS.load_file("modules/jokers/dreamJoker.lua")
 SMODS.load_file("utils/main_util.lua")()
 
 SMODS.Atlas { key = "cardSleeves", path = "atlasSleeves.png", px = 73, py = 95}
-SMODS.Atlas { key = "jokers", path = "Jokers.png", px = 71, py = 95}
+SMODS.Atlas { key = "jokers", path = "atlasJokers.png", px = 71, py = 95}
 SMODS.Atlas { key = "erroredCards", path = "atlasErrored.png", px = 65, py = 95}
 
 -- THIS SHOULD ALWAYS BE LOADED FIRST SINCE IT CONTAINS THE BASE CARD TYPES THAT IS LOADED LATER
